fd27475f7559359965826b01c54fef906a48cdc7
melha <melhamra98@gmail.com>
2021-01-22 16:28:44 +0100

    refactoring my nav-bar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,34 @@
+import { AccountService } from './../../services/account.service';
+import { TokenService } from './../../services/token.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  currentUser: any;
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenService,
+    private accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.accountService.authStatus.subscribe((res) => {
+      this.currentUser = this.tokenService.getInfos();
+    });
   }
 
+  logout() {
+    this.tokenService.remove();
+    this.accountService.changeStaus(false);
+    this.toastr.error('See you soon', 'Eat it');
+    this.router.navigateByUrl('/login');
+  }
 }
